3ebd6438a60bca0193bebb0611c0ce7468794882
Nickolas Andre <nickolas.sam3@gmail.com>
2022-12-29 15:04:22 -0300

    commit

---
+++ aula11/exe008.js
@@ -0,0 +1,9 @@
+
+var vel = 20.5
+console.log(`a velocidade do seu carro é ${vel}km/h`)
+
+if(vel > 60){
+    console.log(`Você ultrapassou a velocidade permitida`)
+}
+
+console.log(`Dirigar sempre usando o cinto de segurança`)
