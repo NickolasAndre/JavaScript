c1e8ea8916da90690d9727f786f822b449fc5631
Nickolas Andre <nickolas.sam3@gmail.com>
2023-01-18 18:55:40 -0300

    Create objeto01.js

---
+++ aula17/objeto01.js
@@ -0,0 +1,10 @@
+let amigo ={nome:'josé'
+, sexo:'M', 
+peso: 85.7
+, engordar(p=0){
+    console.log('Engordou')
+    this.peso += p
+}}
+
+amigo.engordar(2)
+console.log(`${amigo.nome} pesa ${amigo.peso}`)
